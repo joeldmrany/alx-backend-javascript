86a8eea0961b4d43ae58bded84ba22a45d578242
joeldmrany <joeldmrany1@gmail.com>
2024-11-24 16:20:23 +0200

    task

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((right, wrong) => {
     if (success === true) {
-      right("{ status: 200, body: 'Success' }");
+      right({
+        status: 200,
+        body: 'Success',
+      });
     } else {
       wrong(Error('The fake API is not working currently'));
     }
