8aad89210cca703cbdbaf7b8f8a780885f75b659
joeldmrany <joeldmrany1@gmail.com>
2024-12-01 13:14:27 +0200

    task

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([PhotoRes, UserRes]) => {
       console.log(`${PhotoRes.body} ${UserRes.firstName} ${UserRes.lastName}`);
     })
