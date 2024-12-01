8d24490079809c578d1b85fd08bff1433d890c3a
joeldmrany <joeldmrany1@gmail.com>
2024-12-01 19:49:49 +0200

    task

---
+++ 0x02-ES6-classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom';
+
+const C1 = new ClassRoom(19);
+const C2 = new ClassRoom(20);
+const C3 = new ClassRoom(34);
+
+const initializeRooms = () => [C1, C2, C3];
+
+export default initializeRooms;
