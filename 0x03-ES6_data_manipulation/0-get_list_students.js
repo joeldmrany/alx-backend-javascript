55c4c51fe3bd8879401603637fdf4b033d767847
joeldmrany <joeldmrany1@gmail.com>
2024-12-01 20:56:55 +0200

    task

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+const info1 = {
+  id: 1,
+  firstName: 'Guillaume',
+  location: 'San Francisco',
+};
+const info2 = {
+  id: 2,
+  firstName: 'James',
+  location: 'Columbia',
+};
+const info3 = {
+  id: 5,
+  firstName: 'Serena',
+  location: 'San Francisco',
+};
+
+const getListStudents = () => [info1, info2, info3];
+
+export default getListStudents;
