f8ad8fddc19cca9ed4a2db4408d77adf5aab3222
joeldmrany <joeldmrany1@gmail.com>
2025-01-07 07:04:31 +0200

    task: 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const fs = require('fs');
+
+const PORT = 1245;
+const HOST = 'localhost';
+
+// counting number of students in a file
+const countStudents = (File) => new Promise((res, rej) => {
+  fs.readFile(File, 'utf8', (error, info) => {
+    if (error) {
+      rej(new Error('Cannot load the database'));
+    } else {
+      const fileLines = info.trim().split('\n');
+      const fileFields = fileLines[0].split(',');
+      const students = fileLines.slice(1).map((line) => {
+        const value = line.split(',');
+        const student = {};
+        fileFields.forEach((field, index) => {
+          student[field] = value[index];
+        });
+        return student;
+      });
+
+      const CSStudents = students.filter((student) => student.field === 'CS');
+      const SWEStudents = students.filter(
+        (student) => student.field === 'SWE',
+      );
+
+      let response = `Number of students: ${students.length}\n`;
+      response += `Number of students in CS: ${
+        CSStudents.length
+      }. List: ${CSStudents
+        .map((student) => student.firstname)
+        .join(', ')}\n`;
+      response += `Number of students in SWE: ${
+        SWEStudents.length
+      }. List: ${SWEStudents.map((student) => student.firstname).join(', ')}`;
+
+      res(response);
+    }
+  });
+});
+
+// creating server
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Hello ALX!');
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then((response) => {
+        res.end(`This is the list of our students\n${response}`);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.end(err.message);
+      });
+  }
+});
+
+// listening to the server
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
+});
