bd3cec221fcf0117d9c1f167daaba8be27307747
joeldmrany <joeldmrany1@gmail.com>
2025-02-23 09:14:47 +0200

    Dont consider that

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  budget[0] = `income-${getCurrentYear()} : ${income}`;
-  budget[1] = `gdp-${getCurrentYear()} : ${gdp}`;
-  budget[2] = `capita-${getCurrentYear()} : ${capita}`;
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 
   return budget;
 }
